acaf865bd45daf1b09a008e7725b22001f7adf19
Pedro Toledo <pedrot1811@gmail.com>
2025-02-25 16:19:56 -0300

    second challenge missing form refactor

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # get "tasks", to: "tasks#index", as: "tasks"
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id", to: "tasks#show", as: "task"
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#patch"
+  # delete "tasks/:id", to: "tasks#destroy", as: "delete"
+  resources :tasks
 end
